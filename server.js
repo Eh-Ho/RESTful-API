48eef580279d50a4529f02c7b90fbc43671d9a2b
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-10-29 18:15:59 +0330

    connection to database init

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-global.config = require('./modules/config');
 const webRouter = require('./routes/web')
+const mongoose = require('mongoose');
+global.config = require('./modules/config');
 
+//connecting database
+const dbName = 'API_project'
+mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`);
+mongoose.Promise = global.Promise;
+
+
+
+
+
+// express app
 const app = express();
 
+
+
+
+// middlewares and routers
 app.use(bodyParser.urlencoded({extended  : false}));
 
 app.use(bodyParser.json({type : 'application/json'}));
 
-
 app.use('/', webRouter)
 
+
+//server running
 app.listen(config.port, ()=>{
     console.log(`server running on port ${config.port}`);
 });
