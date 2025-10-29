a6ea98d1138a85df8b06bdda94c0b128b59df969
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-10-29 14:07:59 +0330

    body-parser middlewares added to pipeline

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ global.config = require('./modules/config');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended  : false}));
+
+app.use(bodyParser.json({type : 'application/json'}));
+
 app.listen(config.port, ()=>{
     console.log(`server running on port ${config.port}`);
 });
