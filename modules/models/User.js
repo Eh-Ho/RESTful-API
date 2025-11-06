7ce2c96c144d8d0932a6caaba211bf6e75db2671
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-11-06 18:51:35 +0330

    courses list added to user schema

--- modules/models/User.js
+++ modules/models/User.js
@@ -6,7 +6,8 @@ const UserSchema = new Schema(
     {
         name : {type : String, required : true},
         email : {type : String, required : true},
-        password : {type : String, required : true}
+        password : {type : String, required : true},
+        courses : [{type : Schema.Types.ObjectId, ref : 'Course'}]
     },{
         timestamps:{
             createdAt: 'created_at', 
