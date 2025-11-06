fe7f609e91a1438bf4ff0acd46b0db1706670890
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-11-06 19:43:34 +0330

    admin controllers bugs fixed

--- modules/controllers/api/v1/admin/adminCourseController.js
+++ modules/controllers/api/v1/admin/adminCourseController.js
@@ -6,38 +6,38 @@ module.exports = new class AdminCourseController extends Controller{
     async index(req, res){
         try{
             let courses = await this.model.Course.find({});
-            res.json(courses);
+            res.status(200).json(courses);
 
         }catch(err){
-            res.send(err);
+            res.status(500).send(err);
         }
         
     }
 
     async single(req, res){
         try{
-            let course = await this.model.Course.findById(req.params.id);
-            res.json(course);
+            let course = await this.model.Course.findById(req.params.courseId);
+            res.status(200).json(course);
         }catch (err) {
-            res.send(err);
+            res.status(500).send(err);
         }
     }
 
     async store(req, res){
         try{
             let newCourse = new this.model.Course(req.body);
-            res.json(await newCourse.save());
+            res.status(200).json(await newCourse.save());
         } catch(err) {
-            res.send(err);
+            res.status(500).send(err);
         }
     }
 
 
     async update(req, res){
         try {
-            res.json(await this.model.Course.findByIdAndUpdate(req.params.id, req.body, {new : true}));
+            res.status(200).json(await this.model.Course.findByIdAndUpdate(req.params.courseId, req.body, {new : true}));
         } catch (err) {
-            req.send(err);
+            res.status(500).send(err);
         }
         
     }
@@ -45,9 +45,9 @@ module.exports = new class AdminCourseController extends Controller{
 
     async destroy(req, res){
         try{
-            res.json(await this.model.Course.findByIdAndDelete(req.body.id));
+            res.status(200).json(await this.model.Course.findByIdAndDelete(req.params.courseId));
         } catch(err) {
-            res.send(err);
+            res.status(500).send(err);
         }
     }
 
--- modules/controllers/api/v1/admin/adminEpisodeController.js
+++ modules/controllers/api/v1/admin/adminEpisodeController.js
@@ -6,17 +6,28 @@ module.exports = new class AdminEpisodeController extends Controller {
         try{
             let episodes = await this.model.Episode.find({});
             res.status(200).json(episodes);
+
         }catch (err) {
             res.status(500).send(err);
         };
         
     };
 
+    async courseIndex (req, res) {
+        try{
+            let episodes = await this.model.Course.findById(req.params.courseId).populate('episodes');
+            res.status(200).json(episodes);
+
+        }catch (err) {
+            res.status(500).send(err);
+        }
+    }
+
     async store (req, res) {
         try{
             let newEpisode = new this.model.Episode(req.body);
             await newEpisode.save();
-            await this.model.Course.findByIDAndUpdate(req.params.id, {$addToSet : {episodes : newEpisode._id}});
+            await this.model.Course.findByIdAndUpdate(req.params.courseId, {$addToSet : {episodes : newEpisode._id}}, {new : true});
             res.status(200).json(newEpisode);
 
         }catch (err) {
@@ -27,26 +38,31 @@ module.exports = new class AdminEpisodeController extends Controller {
 
     async single (req, res) {
         try{
-            let episode = await this.model.Episode.findById(req.params.id);
+            let episode = await this.model.Episode.findById(req.params.episodeId);
             res.status(200).json(episode);
+
         }catch (err) {
             res.status(500).send(err);
         };
     };
 
-    async update () {
+    async update (req, res) {
         try{
-            res.json(await this.model.Episode.findByIdAndUpdate(req.params.id, req.body, {new : true}));
+            res.json(await this.model.Episode.findByIdAndUpdate(req.params.episodeId, req.body, {new : true}));
+
         }catch (err) {
-            res.status(200).status(500).send(err);
+            res.status(500).send(err);
         };
     };
 
-    async destroy () {
+    async destroy (req, res) {
         try{
-            res.json(await this.model.Episode.findByIdAndDelete(req.body.id))
+            let episode = await this.model.Episode.findById(req.params.episodeId);
+            await this.model.Episode.findByIdAndDelete(req.params.episodeId);
+            res.status(200).json(await this.model.Course.findByIdAndUpdate(episode.course, {$pull : {episodes : req.params.episodeId}}, {new : true}));
+
         }catch (err) {
-            res.status(200).status(500).send(err);
+            res.status(500).send(err);
         };
     };
 
