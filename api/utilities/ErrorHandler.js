face6972af0e39806fe265768127b11b29d30a9f
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-11-09 19:45:48 +0330

    error handler middleware and error handler class created

---
+++ api/utilities/ErrorHandler.js
@@ -0,0 +1,10 @@
+module.exports = new class ErrorHandler extends Error{
+    constructor(message, statusCode){
+        super(message);
+        this.status = statusCode;
+        this.isOperational = true;
+        Error.captureStackTrace(this, this.constructor);
+
+    };
+
+};
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ global.config = require('./api/config');
 const bodyParser = require('body-parser');
 const APIRouter = require('./api/v1/routes');
 const mongoose = require('mongoose');
-
+const {StatusCodes} = require('http-status-codes');
 //connecting database
 const mongoURI = `mongodb://${config.database.username}:${config.database.password}@${config.database.host}/${config.database.databaseName}?authSource=${config.database.authSource}`;
 mongoose.connect(mongoURI)
@@ -26,6 +26,43 @@ app.use(bodyParser.json({type : 'application/json'}));
 
 // routers
 app.use('/', APIRouter);
+
+// error handler
+app.use((err, req, res, next)=>{
+  if(err.isOperational){
+    return res.status(err.statusCode).json({
+      message : err.message
+    });
+  };
+
+  if(error.name === 'CastError'){
+    const message = `Invalid value for ${err.path}: ${err.value}`;
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: message
+    });
+  };
+
+  if (err.name === 'ValidationError') {
+    const messages = Object.values(err.errors).map(val => val.message);
+    const message = `Invalid input data: ${messages.join('. ')}`;
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: message
+    });
+  };
+
+  console.error('UNKNOWN ERROR ----------------');
+  console.error(err);
+  console.error('---------------------------------');
+  
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    message: 'Something went wrong',
+  });
+
+});
+
+
+
+
 
 
 //server running
