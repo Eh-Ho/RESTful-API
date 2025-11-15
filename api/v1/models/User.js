bf87492491453c4746e6887fbcab02db8b77c8b9
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-11-15 10:10:46 +0330

    unique property added to User model

--- api/v1/models/User.js
+++ api/v1/models/User.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema(
     {
         name : {type : String, required : true},
-        email : {type : String, required : true},
+        email : {type : String, required : true, unique : true},
         password : {type : String, required : true},
         courses : [{type : Schema.Types.ObjectId, ref : 'Course'}]
     },{
