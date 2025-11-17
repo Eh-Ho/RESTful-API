72fd04b6beb79b16699bff1c68a2864f6a790518
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-11-17 18:32:42 +0330

    role added to user model

--- api/v1/models/User.js
+++ api/v1/models/User.js
@@ -7,8 +7,10 @@ const UserSchema = new Schema(
         name : {type : String, required : true},
         email : {type : String, required : true, unique : true},
         password : {type : String, required : true},
-        courses : [{type : Schema.Types.ObjectId, ref : 'Course'}]
-    },{
+        courses : [{type : Schema.Types.ObjectId, ref : 'Course'}],
+        role : {type : String, required : true}
+    },
+    {
         timestamps:{
             createdAt: 'created_at', 
             updatedAt: 'updated_at'
