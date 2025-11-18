c0b50b7e7980c1dcae17a6245a73f685630d2554
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-11-18 21:06:11 +0330

    public routes for auth completed

--- api/v1/routes/publicRouter.js
+++ api/v1/routes/publicRouter.js
@@ -2,17 +2,16 @@ const express = require('express');
 const userRouter = require('./userRouter');
 
 const publicRouter = express.Router();
-const {HomeController} = require(`${config.path.controllers}`);
-
-
+const {HomeController, AuthController} = require(`${config.path.controllers}`);
+const AuthValidator = require(`${config.path.validators}`);
 // home
 userRouter.route('/').get(HomeController.index.bind(HomeController));
 
 
 // auth
-userRouter.route('/register').post();
-userRouter.route('/signin').post();
-userRouter.route('/signout').post();
+userRouter.route('/register').post(AuthValidator.validateRegister, AuthValidator.handleValidationErrors, AuthController.register.bind(AuthController));
+userRouter.route('/signin').post(AuthValidator.validateSignIn, AuthValidator.handleValidationErrors, AuthController.signIn.bind(AuthController));
+userRouter.route('/signout').post(AuthController.signOut.bind(AuthController));
 
 
 module.exports = publicRouter;
